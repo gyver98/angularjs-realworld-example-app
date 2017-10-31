f6bc41bb46ea58b631f9d4278e391d8e39f0ef5a
Matthew Choi <matthewchoi7@gmail.com>
2017-10-31 13:49:49 +1100

    adding AuthConfig

---
+++ src/js/auth/auth.config.js
@@ -0,0 +1,17 @@
+function AuthConfig($stateProvider, $httpProvider) {
+  'ngInject';
+  // Define the routes
+  $stateProvider
+  .state('app.login', {
+    url: '/login',
+    template: 'auth/auth.html',
+    title: 'Sign in'
+  })
+  .state('app.register', {
+    url: '/register',
+    templateUrl: 'auth/auth.html',
+    title: 'Sign up'
+  });
+};
+
+export default AuthConfig;
